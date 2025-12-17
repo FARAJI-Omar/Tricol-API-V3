dd2c62a6a0da887f852b494431fe8dfcbb2c4187
F99 <farajiomar99@gmail.com>
2025-12-17 12:13:43 +0100

    initial commit: v3 based on v2

---
+++ src/main/java/com/example/tricol/tricolspringbootrestapi/model/OrderItem.java
@@ -0,0 +1,46 @@
+package com.example.tricol.tricolspringbootrestapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.repository.cdi.Eager;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double quantity;
+
+    private Double unitPrice;
+
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                ", total=" + total +
+                ", order=" + order +
+                ", product=" + product +
+                '}';
+    }
+}
