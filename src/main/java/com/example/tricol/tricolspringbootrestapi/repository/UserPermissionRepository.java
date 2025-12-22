8488a8651ac2e245e559eebeddf78574e5965a74
F99 <farajiomar99@gmail.com>
2025-12-22 09:21:40 +0100

    feat: update repositories to enhance permission and user permission queries

--- src/main/java/com/example/tricol/tricolspringbootrestapi/repository/UserPermissionRepository.java
+++ src/main/java/com/example/tricol/tricolspringbootrestapi/repository/UserPermissionRepository.java
@@ -1,13 +1,17 @@
 package com.example.tricol.tricolspringbootrestapi.repository;
 
+import com.example.tricol.tricolspringbootrestapi.model.Permission;
 import com.example.tricol.tricolspringbootrestapi.model.UserApp;
 import com.example.tricol.tricolspringbootrestapi.model.UserPermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserPermissionRepository extends JpaRepository<UserPermission, Long> {
     List<UserPermission> findByUser(UserApp user);
+    Optional<UserPermission> findByUserAndPermission(UserApp user, Permission permission);
+    Optional<UserPermission> findByUserIdAndPermissionId(Long userId, Long permissionId);
 }
