6a2c826c2c3fad7fb91ac6d89656ede523ba62b1
F99 <farajiomar99@gmail.com>
2026-01-05 00:36:32 +0100

    feat: add Keycloak token response and refresh token request DTOs

---
+++ src/main/java/com/example/tricol/tricolspringbootrestapi/dto/response/KeycloakTokenResponse.java
@@ -0,0 +1,36 @@
+package com.example.tricol.tricolspringbootrestapi.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KeycloakTokenResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private Long expiresIn;
+
+    @JsonProperty("refresh_expires_in")
+    private Long refreshExpiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("not-before-policy")
+    private Long notBeforePolicy;
+
+    @JsonProperty("session_state")
+    private String sessionState;
+
+    @JsonProperty("scope")
+    private String scope;
+}
